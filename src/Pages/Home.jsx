92d465cecd696b2ba86971947b7de74f8a674a87
mehedi <darkhasan10@gmail.com>
2024-02-27 03:51:22 +0530

    make the home page responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,16 +3,16 @@ import Subject from "../Components/Subject"
 
 function Home() {
   return (
-    <div className="home w-screen h-screen bg-slate-600 flex">
-        <div className=" h-full w-[55%] flex justify-center place-items-center">
-            <section className=" h-[40%] w-[60%] text-white pl-8">
-              <p className=" text-5xl font-thin">Wellcome to the</p>
-              <p className=" text-5xl font-bold">Frontend Quiz!</p>
-              <p className=" mt-10 font-semibold font-mono">Pick a subject to get start</p>
+    <div className="home w-screen h-screen bg-slate-600 flex sm:flex-row flex-col">
+        <div className=" h-full sm:w-[55%] w-full flex justify-center place-items-center">
+            <section className=" h-[40%] sm:w-[60%] w-full text-white sm:pl-8 flex flex-col justify-center place-items-center mt-10 sm:mt-0">
+              <p className=" sm:text-5xl text-4xl font-thin">Wellcome to the</p>
+              <p className=" sm:text-5xl text-4xl font-bold">Frontend Quiz!</p>
+              <p className=" sm:mt-10 mt-6 font-semibold font-mono">Pick a subject to get start</p>
             </section>
         </div>
-        <div className=" h-full w-[45%] flex sm:justify-start place-items-center">
-          <section className=" h-[50%] w-[50%] sm:ml-8 flex flex-col justify-center place-items-center gap-10">
+        <div className=" h-full sm:w-[45%] w-full flex sm:justify-start sm:place-items-center place-items-start justify-center">
+          <section className=" sm:h-[50%] h-[70%] sm:w-[50%] w-[90%] sm:ml-8 flex flex-col justify-center place-items-center gap-8 sm:gap-10 mr-2 sm:mr-0">
             <Subject name="HTML" src = "https://cdn-icons-png.flaticon.com/512/1051/1051277.png"/>
             <Subject name="CSS" src = "https://cdn-icons-png.flaticon.com/512/732/732190.png"/>
             <Subject name = "JAVASCRIPT" src = "https://cdn-icons-png.flaticon.com/512/5968/5968292.png"/>
--- src/Pages/Result.jsx
+++ src/Pages/Result.jsx
@@ -2,7 +2,7 @@
 function Result() {
   return (
     <div className="result w-screen h-screen flex justify-center place-items-center text-white">
-        <section className=" w-[50%] h-[70%] flex flex-col justify-center place-items-center gap-2">
+        <section className=" w-[50%] h-[70%] flex flex-col justify-center place-items-center gap-3">
             <h1 className=" h-[15%] w-full flex justify-center place-items-center text-3xl font-black">Your total Score..</h1>
             <div className=" w-[60%] h-[65%] bg-gray-700 rounded-lg">
                 <section className=" w-full h-[20%] flex justify-center place-items-center">
@@ -12,7 +12,7 @@ function Result() {
                 <section className=" w-full h-[60%] flex justify-center place-items-center text-[10rem] font-semibold">4</section>
                 <section className=" w-full h-[20%] flex justify-center place-items-center text-3xl font-semibold">out of 5</section>
             </div>
-            <button className=" w-[65%] h-[15%] bg-purple-900 rounded-lg text-3xl font-bold cursor-pointer">Play Again</button>
+            <button className=" w-[60%] h-[15%] bg-purple-900 rounded-lg text-3xl font-bold cursor-pointer">Play Again</button>
         </section>
     </div>
   )
